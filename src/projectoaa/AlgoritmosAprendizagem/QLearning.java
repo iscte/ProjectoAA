d6ed911df81a19c5fc1c9bb38f271f4cd26b4b5b
Anibal Caixinha <anibalcaixinha@gmail.com>
2011-12-17 16:36:22 +0000

    Initial commit

---
+++ src/projectoaa/AlgoritmosAprendizagem/QLearning.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectoaa.AlgoritmosAprendizagem;
+
+/**
+ *
+ * @author Bruno
+ */
+public class QLearning {
+    public int s, a = 0;
+    public int[][] qArray = new int[100][100];
+    
+    // to be continued... //
+}
